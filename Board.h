1c9394f4f919b948d33924bbf5ec9b461cb495bd
gendestry <zan.oberstar@gmail.com>
2017-08-16 18:08:29 +0200

    Added Pathfinder

--- Board.h
+++ Board.h
@@ -4,12 +4,19 @@
 #include <iostream>
 #include <ctime>
 
-#define CELL_SIZE 20	// inverted difficulty the higher it is the easier the level
+#define CELL_SIZE 40	// inverted difficulty the higher it is the easier the level
 #define WALL_THICKNES 1
 
+#define START_X 0
+#define START_Y 0
+#define END_X (m_Width / CELL_SIZE)
+#define END_Y (m_Height / CELL_SIZE)
+
 struct Cell {
-	bool walls[4];// top right bottom left
-	bool visited = false;
+	bool walls[4];			// top right bottom left
+	bool visited = false;   // for the mazeGen
+	bool marked = false;	// if patfinder was on the cell
+	bool correct = false;	// if the cell is correct
 	int x, y;
 
 	Cell() {
@@ -33,19 +40,28 @@ private:
 	std::vector<Cell> stack;
 
 	sf::RectangleShape rect, horizontalWall, verticalWall;
+	sf::CircleShape circle;
 
 	Cell* current;
 	Cell* nextC;
 public:
-	bool done = false;
+	bool mazeGenDone = false;
+	bool allDone = false;
 	bool error = false;
 
 	Board(int screenWidth, int screenHeight);
 	~Board();
 
-	void render(sf::RenderWindow &window);
-	void update();
-	inline int getIndex(int x, int y);
-	int nextNeighbour(); // returns index of nextneighbour (randomly)
+	// uses maze generator
+	int nextNeighbour();				// returns index of the next neighbour (randomly)
 	void removeWalls(int nextIndex);
+	void genMaze();
+
+	void render(sf::RenderWindow &window);
+
+	inline int getIndex(int x, int y);
+
+	// pathfinder functions
+	int pathFindNextNeighbour();
+	void findPath();				
 };
