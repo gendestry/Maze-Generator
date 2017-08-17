566bb969ecb46d59bcbf5b568ba8875ccf1a1b48
gendestry <zan.oberstar@gmail.com>
2017-08-17 11:50:20 +0200

    Added error handling

--- main.cpp
+++ main.cpp
@@ -4,34 +4,44 @@
 const int SCR_WIDTH = 800;
 const int SCR_HEIGHT = 600;
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(SCR_WIDTH, SCR_HEIGHT), "Maze Generator");
-	sf::Event event;
+int main() {
+	try {
+		sf::RenderWindow window(sf::VideoMode(SCR_WIDTH, SCR_HEIGHT), "Maze Generator");
+		sf::Event event;
 
-	Board board(SCR_WIDTH, SCR_HEIGHT);
-	// window.setFramerateLimit(10);
+		Board board(SCR_WIDTH, SCR_HEIGHT);
+		// window.setFramerateLimit(10);
 
-	while (window.isOpen()) {
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-				window.close();
+		while (window.isOpen()) {
+			while (window.pollEvent(event)) {
+				if (event.type == sf::Event::Closed)
+					window.close();
+				if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+
+			window.clear();
+
+			// drawing
+			//if (!board.error) {
+				if (!board.mazeGenDone)
+					board.genMaze();
+				else if (!board.allDone)
+					board.findPath();
+				board.render(window);
+			//}
+
+			window.display();
 		}
-
-		window.clear();
-
-		// drawing
-		if (!board.error) {
-			if (!board.mazeGenDone)
-				board.genMaze();
-			else if (!board.allDone)
-				board.findPath();
-			board.render(window);
-		}
-
-		window.display();
+	}
+	catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << "!!!\n";
+		system("pause");
+		return -1;
+	}
+	catch (...) { // this actually says ..., I'm not abbreviating :P
+		std::cerr << "Unknown error.\n";
+		return -1;
 	}
 	return 0;
 }
